029c987fe0cbf9edd949eb8739308bd5a7d9e498
YiK00 <kuangyi00@gmail.com>
2018-11-02 00:41:58 -0400

    Update POM.R

--- POM.R
+++ POM.R
@@ -34,23 +34,74 @@ p_value2 <- pnorm(abs(ctable2[, "t value"]),
 ctable2 <- cbind(ctable2, "p value" = p_value2)
 ctable2
 
-predict(pom, newdata = data.frame(party = "Dem"), type = "p")
-predict(pom, newdata = data.frame(party = "Rep"), type = "p")
-
-# Democrat cumulative probabilities
+# NxCdk8 cumulative probabilities
 Cdk8_cp <- exp(POM2$zeta - POM2$coefficients)/(1 + exp(POM2$zeta - POM2$coefficients))
-# Republican cumulative probabilities
+# Nhet cumulative probabilities
 N_cp <- exp(POM2$zeta)/(1 + exp(POM2$zeta))
-N_cp
 
-# Democrat odds
+# NxCdk8 odds
 Cdk_odds <- (dcp/(1-dcp))
-dodds
-# Repuclican odds
+# Nhet odds
 N_odds <- (N_cp/(1-N_cp))
-N_odds
+
+# odds ratio
+OR <- N_odds/Cdk8_odds
+OR
 
 
+# p value adjustment
+library(FSA)
+Geno2 <- c("ago1", "ago3", "cdk8", 
+           "cycC", "kto241", "kto631", 
+           "skd13", "skd413", 
+           "b1", "b2", "b3")
+p_value2 <- data.frame(Geno2, p_value2)
+headtail(p_value2)
+
+# perform p value adjustment and add to dataframe
+p_value2$Bonferroni <- p.adjust(p_value2$p_value2, 
+                                method = "bonferroni")
+p_value2$BH <- p.adjust(p_value2$p_value2, 
+                                method = "BH")
+p_value2$Holm <- p.adjust(p_value2$p_value2, 
+                                method = "holm")
+p_value2$Hommel <- p.adjust(p_value2$p_value2, 
+                                method = "hommel")
+p_value2$BY <- p.adjust(p_value2$p_value2, 
+                                method = "BY")
+p_value2
+
+
+# plot
+X = p_value2$p_value2
+Y = cbind(p_value2$Bonferroni,
+          p_value2$BH,
+          p_value2$Holm,
+          p_value2$Hochberg,
+          p_value2$Hommel,
+          p_value2$BY)
+
+matplot(X, Y,
+        xlab="Raw p-value",
+        ylab="Adjusted p-value",
+        type="l",
+        asp=1,
+        col=1:6,
+        lty=1,
+        lwd=2)
+
+legend('bottomright', 
+       legend = c("Bonferroni", "BH", "Holm", "Hochberg", "Hommel", "BY"), 
+       col = 1:6, 
+       cex = 1,    
+       pch = 16)
+
+abline(0, 1,
+       col=1,
+       lty=2,
+       lwd=1)
+
+# 1 variable test ---------------------------------------------------------
 
 
 N <- c(111,	36,	13,	2)
